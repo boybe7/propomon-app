dadac3b0dd4473cff5b7b65d1a774e933b3aac63
boybe7 <boybe7oo@gmail.com>
2016-08-24 11:39:42 +0700

    First commit

---
+++ ProPomon/ProPomon/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProPomon
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void MonsterInfo(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MonsterInfo());
+        }
+    }
+}
